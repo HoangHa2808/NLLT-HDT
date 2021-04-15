5a6a300f3bff561946d8cc8d7aff42b455cc4222
HoangHa2808 <hadoan12a1@gmail.com>
2021-04-15 18:08:59 +0700

    ban hoan thien

---
+++ QuanLyHinhHoc1/QuanLyHinhHoc1/HinhHoc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLyHinhHoc1
+{
+    class HinhHoc
+    {
+        public virtual float TinhDT()
+        {
+            return 0;
+        }
+        public virtual float TinhCV()
+        {
+            return 0;
+        }
+
+        public virtual string KieuHinhHoc()
+        {
+            return KieuHinhHoc();
+        }
+    }
+
+}
+
---
+++ QuanLyHinhHoc1/QuanLyHinhHoc1/HinhVuong.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLyHinhHoc1
+{
+    class HinhVuong : HinhHoc
+    {
+
+        public float canh;
+
+        public HinhVuong()
+        {
+        }
+
+        public HinhVuong(float c)
+        {
+            canh = c;
+        }
+
+        public override string KieuHinhHoc()
+        {
+            return KieuHinhHoc();
+        }
+        public override float TinhCV()
+        {
+            return (float)Math.Round(canh * 4);
+        }
+
+        public override float TinhDT()
+        {
+            return (float)Math.Round(canh * canh, 0);
+        }
+
+        public override string ToString()
+        {
+            //return "Hinh vuong co canh = " + canh + " chu vi = "+ TinhCV()+ " dien tich = " + TinhDT();
+            return string.Format("Hinh vuong:\n\t co canh = {0}, chu vi = {1}, dien tich = {2}", canh, TinhCV(), TinhDT());
+        }
+    }
+}
